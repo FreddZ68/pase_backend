cab47862724dad6cb8b5a8789f9ca9567f0a8c21
FreddZ68 <fredd68flores@gmail.com>
2026-09-09 00:30:47 -0600

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -179,6 +179,7 @@ app.delete("/utensilios/:id", (req, res) => {
 // AlwaysData (y la mayoría de hostings) asignan el puerto dinámicamente vía
 // variable de entorno; en tu máquina local, sin esa variable, sigue usando 3000.
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Servidor ejecutándose en el puerto ${PORT}`);
 });
